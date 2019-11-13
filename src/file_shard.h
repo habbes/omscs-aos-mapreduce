8ab28dc6a8ff223f31c67ec4e39f10210af13bbd
Clément Habinshuti <hello@habbes.xyz>
2019-11-13 22:53:11 +0300

    Implement file sharding

--- src/file_shard.h
+++ src/file_shard.h
@@ -1,17 +1,92 @@
 #pragma once
 
+#include <string>
 #include <vector>
+#include <cstdio>
 #include "mapreduce_spec.h"
 
+struct FileShardOffset {
+     std::string file;
+     int start;
+     int stop;
+
+     bool operator==(const FileShardOffset & other) const {
+          return this->file == other.file && this->start == other.start && this->stop == other.stop;
+     }
+};
 
 /* CS6210_TASK: Create your own data structure here, where you can hold information about file splits,
      that your master would use for its own bookkeeping and to convey the tasks to the workers for mapping */
 struct FileShard {
+     std::vector<FileShardOffset> offsets;
 
+     bool operator==(const FileShard & other) const {
+          return this->offsets == other.offsets;
+     }
+
+     void reset() {
+          offsets.clear();
+     }
 };
+
+int get_file_size(FILE *file)
+{
+     if (!file) return -1;
+     fseek(file, 0, SEEK_END);
+     int size = ftell(file);
+     fseek(file, 0, SEEK_SET);
+     return size;
+}
+
+int get_file_next_boundary(FILE *file, const int pos)
+{
+     fseek(file, pos, SEEK_SET);
+     int boundary_pos = pos;
+     char buf;
+     while (!feof(file)) {
+          fread(&buf, sizeof(char), 1, file);
+          boundary_pos += 1;
+          if (buf == '\n') break;
+     }
+     return boundary_pos;
+}
 
 
 /* CS6210_TASK: Create fileshards from the list of input files, map_kilobytes etc. using mr_spec you populated  */ 
 inline bool shard_files(const MapReduceSpec& mr_spec, std::vector<FileShard>& fileShards) {
+     int shard_size = mr_spec.map_kilobytes * 1024;
+     FileShard current_shard;
+     FILE *file;
+     int current_shard_remaining = shard_size;
+
+     for (const auto filename : mr_spec.input_files) {
+          file = fopen(filename.c_str(), "r");
+          auto file_size = get_file_size(file);
+          if (current_shard_remaining >= file_size) {
+               current_shard.offsets.push_back({.file = filename, .start = 0, .stop = file_size - 1});
+               current_shard_remaining -= file_size;
+               fclose(file);
+               continue;
+          }
+          auto current_file_pos = 0;
+          while (current_file_pos < file_size) {
+               int boundary = get_file_next_boundary(file, current_file_pos + current_shard_remaining - 1);
+               boundary = boundary > file_size ? file_size : boundary;
+               current_shard_remaining -= boundary - current_file_pos;
+               current_shard.offsets.push_back({.file = filename, .start = current_file_pos, .stop = boundary - 1});
+               
+               if (current_shard_remaining <= 0) {
+                    fileShards.push_back(std::move(current_shard));
+                    current_shard_remaining = shard_size;
+               }
+
+               current_file_pos = boundary;
+          }
+          
+          fclose(file);
+     }
+     if (!current_shard.offsets.empty()) {
+          fileShards.push_back(std::move(current_shard));
+     }
 	return true;
 }
--- unit_tests/tests.cc
+++ unit_tests/tests.cc
@@ -1,12 +1,24 @@
 #include <iostream>
 #include <vector>
 #include "../src/mapreduce_spec.h"
+#include "../src/file_shard.h"
 
-#define TEST(expr, msg) if (!(expr)) { fprintf(stderr, "Test failed: %s\n", msg); return EXIT_FAILURE;}
+#define TEST(expr, msg) if (!(expr)) { fprintf(stderr, "Test failed: %s\n", msg); return false;}
 
-int main()
+template <typename T>
+inline bool are_equal(T actual, T expected)
 {
-    std::cout << "Running tests..." << std::endl;
+    if (actual != expected) {
+        std::cerr << "Expected " << actual << " to equal " << expected << std::endl;
+        return false;
+    }
+    return true;
+}
+
+#define RUN_TESTS(fn) if (!(fn())) return false;
+
+bool test_read_mr_spec_from_config_file()
+{
     MapReduceSpec spec;
     read_mr_spec_from_config_file("./config.ini", spec);
 
@@ -27,6 +39,73 @@ int main()
     };
     TEST(spec.input_files == expected_in_files, "parsed input files");
 
+    return true;
+}
+
+bool test_shard_files()
+{
+    MapReduceSpec spec;
+    spec.map_kilobytes = 200;
+    spec.input_files.push_back("./testdata_1.txt");
+    spec.input_files.push_back("./testdata_2.txt");
+    spec.input_files.push_back("./testdata_3.txt");
+
+    std::vector<FileShard> shards;
+    shard_files(spec, shards);
+
+    FileShard shard_1, shard_2, shard_3;
+    shard_1.offsets.push_back({.file = "./testdata_1.txt", .start = 0, .stop = 204850});
+
+    shard_2.offsets.push_back({.file = "./testdata_1.txt", .start = 204851, .stop = 342122});
+    shard_2.offsets.push_back({.file = "./testdata_2.txt", .start = 0, .stop = 43120});
+    shard_2.offsets.push_back({.file = "./testdata_3.txt", .start = 0, .stop = 24502});
+
+    shard_3.offsets.push_back({.file = "./testdata_3.txt", .start = 24503, 171419});
+
+    std::vector<FileShard> expectedShards = { shard_1, shard_2, shard_3 };
+    // TEST(shards == expectedShards, "decoded shards");
+    TEST(are_equal(shards.size(), expectedShards.size()), "shards length correct");
+    for (int i = 0; i < shards.size(); i++) {
+        auto & actual = shards.at(i);
+        auto & expected = expectedShards.at(i);
+        TEST(are_equal(actual.offsets.size(), expected.offsets.size()), "shards offsets length correct");
+        for (int j = 0; j < actual.offsets.size(); j++) {
+            auto & actual_offset = actual.offsets.at(j);
+            auto & expected_offset = expected.offsets.at(j);
+            TEST(are_equal(actual_offset.file, expected_offset.file), "shard offset file");
+            TEST(are_equal(actual_offset.start, expected_offset.start), "shard offset start");
+            TEST(are_equal(actual_offset.stop, expected_offset.stop), "shard offset stop");
+        }
+    }
+
+    return true;
+}
+
+bool test_shard_files_with_one_shard_one_file()
+{
+    MapReduceSpec spec;
+    spec.map_kilobytes = 500;
+    spec.input_files.push_back("./testdata_1.txt");
+
+    std::vector<FileShard> shards;
+    shard_files(spec, shards);
+
+    TEST(are_equal((int) shards.size(), 1), "shards length");
+    TEST(are_equal(shards[0].offsets.size(), (size_t) 1), "shard offsets length is 1");
+    TEST(are_equal(shards[0].offsets[0].file, std::string("./testdata_1.txt")), "single shard offset file");
+    TEST(are_equal(shards[0].offsets[0].start, 0), "single shard offset start");
+    TEST(are_equal(shards[0].offsets[0].stop, 342122), "single shard offset stop");
+
+    return true;
+}
+
+int main()
+{
+    std::cout << "Running tests..." << std::endl;
+    
+    RUN_TESTS(test_read_mr_spec_from_config_file);
+    RUN_TESTS(test_shard_files);
+    RUN_TESTS(test_shard_files_with_one_shard_one_file);
 
     std::cout << "All tests passed!" << std::endl;
     
